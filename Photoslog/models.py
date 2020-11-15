8180e5e8e282e26224a5f275eccd3465eb977969
REAGAN2020 <reaganteflon@gmail.com>
2020-11-15 18:25:21 +0300

    added class and tested

--- Photoslog/models.py
+++ Photoslog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+import datetime as dt
 # Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length=50)
+    @classmethod
+    def __str__(cls):
+        return self.name
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+    def update_location(self, update):
+        self.name = update
+        self.save()
+
+    @classmethod
+    def get_location_id(cls, id):
+        location = Location.objects.get(pk = id)
+        return location
--- Photoslog/tests.py
+++ Photoslog/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from .models import Location
 
 # Create your tests here.
+
+class LocationTestClass(TestCase):
+    #Setup Method
+    def setUp(self):
+        self.loc = Location(name="Nairobi")
+        self.loc.save_location()
+    
+    def test_instance(self):
+        self.assertTrue(isinstance(self.loc,Location))
+    
+    def test_save_method(self):
+        self.loc.save_location()
+        location = Location.objects.all()
+        self.assertTrue(len(location) > 0)
+    
+    def test_delete_method(self):
+        self.loc.save_location()
+        self.loc.delete_location()
+        location = Location.objects.all()
+        self.assertTrue(len(location) == 0)
+
+    def test_update(self):
+        location = Location.get_location_id(self.loc.id)
+        location.update_location('Nairobi')
+        location  = Location.get_location_id(self.loc.id)
+        self.assertTrue(location.name == 'Nairobi')
