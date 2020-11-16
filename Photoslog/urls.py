36ad44ef8e8bd20f73b5cc83b76735f35ab99874
REAGAN2020 <reaganteflon@gmail.com>
2020-11-16 16:14:00 +0300

    added search functionality

--- Photoslog/urls.py
+++ Photoslog/urls.py
@@ -5,7 +5,8 @@ from . import views
 
 urlpatterns=[
     url('^$', views.index, name='index'),
-  url(r'^image/(?P<category_name>\w+)/(?P<image_id>\d+)',views.single_image,name = 'single_image')
+    url(r'^search/', views.search_photo, name='search_photo'),
+    url(r'^image/(?P<category_name>\w+)/(?P<image_id>\d+)',views.single_image,name = 'single_image')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- Photoslog/views.py
+++ Photoslog/views.py
@@ -19,4 +19,20 @@ def single_image(request,category_name,image_id):
         image = Image.objects.get(id = image_id)
     except DoesNotExist:
         raise Http404()
-    return render(request,"single_image.html",{'title':title,"image":image, "locations":locations, "image_category":image_category})
+    return render(request, "single_image.html", {'title': title, "image": image, "locations": locations, "image_category": image_category})
+
+def search_photo(request):
+    title = 'Search'
+    categories = Category.objects.all()
+    locations = Location.objects.all()
+    if 'Image_category' in request.GET and request.GET['Image_category']:
+        search = request.GET.get('Image_category')
+        found_results = Image.search_by_category(search)
+        message = f"{search}"
+        print(search)
+        print(found_results)
+
+        return render(request, 'search_photo.html',{'title':title,'images': found_results, 'message': message, 'categories': categories, "locations":locations})
+    else:
+        message = 'You havent searched yet'
+        return render(request, 'search_photo.html',{"message": message})
