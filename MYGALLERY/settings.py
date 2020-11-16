e2ca0558bb31a38e746214ccd6ee08a07f76637c
REAGAN2020 <reaganteflon@gmail.com>
2020-11-16 23:34:58 +0300

    added gunicorn

--- MYGALLERY/settings.py
+++ MYGALLERY/settings.py
@@ -44,9 +44,6 @@ DATABASES['default'].update(db_from_env)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
